30c97109476158171fa8867909cbfa226483601b
jkyle109 <jkyle109@gmail.com>
2020-06-11 05:04:37 -0400

    Setup for heroku and local

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 const fs = require("fs");
 const Discord = require("discord.js");
+
+require("dotenv").config();
+
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
 
-//const config = require("./config");
-const prefix = process.env.prefix || config.prefix;
-const token = process.env.token || config.token;
+const prefix = process.env.PREFIX;
+const token = process.env.TOKEN;
 
 for(const file of commandFiles){
     const command = require(`./commands/${file}`);
     client.commands.set(command.name, command);
 }
 
+
 client.on("ready", () => {
     console.log("Ready!");
 });
+
 
 client.on("message", (message) => {
     if(!message.content.startsWith(prefix) || message.author.bot){
