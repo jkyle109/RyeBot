3439a963e202f86789ce75296d48d7852e20b31e
jkyle109 <jkyle109@gmail.com>
2020-10-13 01:00:22 -0400

    Igor's Birthday

--- index.js
+++ index.js
@@ -81,8 +81,10 @@ client.on("message", (message) => {
     }
 
     if(message.author.tag == "Captain Save a Hoe#6969"){
-        message.reply("How are you so handsome?",{files: [igor]})
-        return
+        if(genRand(1,100)%50 == 0){
+            message.reply("How are you so handsome?",{files: [igor]})
+            return
+        }
     }
 
     if(message.author.tag == "kylus#2238"){
